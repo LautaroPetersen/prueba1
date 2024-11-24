262785ab10de331cb1bd695160ab1e9685b4534f
LautaroPetersen <lautaropetersen@gmail.com>
2024-11-24 16:35:02 -0300

    creamos las views y el html

--- prueba1/prueba1/urls.py
+++ prueba1/prueba1/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from .views import saludo, dia_de_hoy, muestra_nombre
+from django.contrib import admin # type: ignore
+from django.urls import path # type: ignore
+from .views import saludo, dia_de_hoy, muestra_nombre, probando_template
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
-    path('diadehoy/', dia_de_hoy )
+    path('diadehoy/', dia_de_hoy),
     path('muestra_nombre/<nombre>/', muestra_nombre),
+    path('probando_template/', probando_template),
 ]
--- prueba1/prueba1/views.py
+++ prueba1/prueba1/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse # type: ignore
 from datetime import datetime
+from django.template import Template, Context # type: ignore
 
 
 def saludo(request):
@@ -11,3 +12,23 @@ def dia_de_hoy(request):
 
 def muestra_nombre(request, nombre):
     return HttpResponse(f"Buenos días {nombre}, bienvenido a Coder")
+
+
+def probando_template(request):
+
+    # Abrimos el archivo html
+    mi_html = open('./practica1/plantilla/index.html')
+
+    # Creamos el template haciendo uso de la clase Template
+    plantilla = Template(mi_html.read())
+
+    # Cerramos el archivo previamente abierto, ya que lo tenemos cargado en la variable plantilla
+    mi_html.close()
+
+    # Creamos un contexto, más adelante vamos a aprender a usarlo, ahora lo necesitamos aunque sea vacío para que funcione
+    mi_contexto = Context()
+
+    # Terminamos de construír el template renderizándolo con su contexto
+    documento = plantilla.render(mi_contexto)
+
+    return HttpResponse(documento)
